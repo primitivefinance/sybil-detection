b2d11fb5ea0c12211d0c6559309b6497e6760b0c
Waylon Jepsen <waylonjepsen1@gmail.com>
2022-07-12 16:36:29 -0600

    graph

---
+++ src/graph_users.py
@@ -0,0 +1,21 @@
+import matplotlib.pyplot as plt
+
+
+def pie_plot(unique_users, sybil_suspects):
+    unique_authentic_users = unique_users - sybil_suspects
+    labels = "Sybil Suspects", "Authentic Users"
+    sizes = [sybil_suspects, unique_authentic_users]
+    explode = (0.1, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+
+    fig1, ax1 = plt.subplots()
+    ax1.pie(
+        sizes,
+        explode=explode,
+        labels=labels,
+        autopct="%1.1f%%",
+        shadow=True,
+        startangle=90,
+    )
+    ax1.axis("equal")  # Equal aspect ratio ensures that pie is drawn as a circle.
+
+    plt.show()
--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from get_data import get_data
 from sybil_check import check_val, get_unique_users
+from graph_users import pie_plot
 from pprint import pprint
 
 
@@ -7,11 +8,13 @@ def main():
     users = get_data()
     sybil_suspects = check_val(users)
     unique_users = get_unique_users(users)
+    print("Total Unique users is {}".format(len(unique_users)))
     print(
         "Unique Users not sybiling is {}".format(
             len(unique_users) - len(sybil_suspects)
         )
     )
+    pie_plot(len(unique_users), len(sybil_suspects))
 
 
 if __name__ == "__main__":
